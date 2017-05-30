09db4854ef64ba821c006a3f413e803dd659df5e
at-haole1 <erichao96@gmail.com>
2017-05-30 17:00:55 +0700

    a=10

--- app/src/main/java/com/example/kimhao/demo/MainActivity.java
+++ app/src/main/java/com/example/kimhao/demo/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    String a = "Xin chao moi nguoi";
-                    mTextMessage.setText(a);
+                    int a = 10;
+                    mTextMessage.setText(""+a);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
